fa10afebbdd8a4ae57c280c4de71ec9f38abb843
aleksandra.mocnik <aleksandra.mocnik@DESKTOP-SEG238F>
2020-11-23 18:37:36 +0100

    Add project files.

---
+++ Domaci/BasicStats.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaci
+{
+    class BasicStats
+    {
+        public int GetLowest(List<int> numbers)
+        {
+            return numbers.Min();
+
+        }
+        public int GetHighest(List<int> numbers)
+        {
+            return numbers.Max();
+
+        }
+        public int GetAverage(List<int> numbers)
+        {
+            return (int)numbers.Average();
+
+        }
+        public int GetSum(List<int> numbers)
+        {
+            return numbers.Sum();
+
+        }
+
+
+    }
+}
---
+++ Domaci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            list.Add(number);
+            Console.WriteLine("Enter another number");
+            number = Convert.ToInt32(Console.ReadLine());
+            list.Add(number);
+            Console.WriteLine("Enter another number");
+            number = Convert.ToInt32(Console.ReadLine());
+            list.Add(number);
+            BasicStats bStats = new BasicStats();
+
+            Console.WriteLine("The average is {0}", bStats.GetAverage(list));
+            Console.WriteLine("The highest is {0}", bStats.GetHighest(list));
+            Console.WriteLine("The lowest is {0}", bStats.GetLowest(list));
+            Console.WriteLine("The sum is {0}", bStats.GetSum(list));
+
+            Console.ReadKey();
+        }
+    }
+}
